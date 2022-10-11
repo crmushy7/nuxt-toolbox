18cce045369da04149b472adf4d0bdf7791f6af0
crmushy7 <112522385+crmushy7@users.noreply.github.com>
2022-10-11 22:19:23 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,19 @@
     <div class="intro">
       <h1>Nuxt Toolbox</h1>
       <p>
-        Hi 👋! This template gives you a
+       hapa-content
         <a href="https://nuxtjs.org/">Nuxt</a> app with the scaffolding for
         <a href="https://www.netlify.com/products/functions/"
           >Netlify Functions</a
         >, <a href="https://www.netlify.com/products/forms/">Forms</a>, and
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
+        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>.hapa-code
       </p>
 
       <p>
-        You can find the code for this project on GitHub at
+        maelezo
         <a href="https://github.com/netlify-templates/nuxt-toolbox"
           >https://github.com/netlify-templates/nuxt-toolbox</a
-        >! Happy coding!
+        >maelezo
       </p>
     </div>
     <FeedbackForm />
