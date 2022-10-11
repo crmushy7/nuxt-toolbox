6d784073149b3a0739c6ebf2cc0d84557f04540b
crmushy7 <112522385+crmushy7@users.noreply.github.com>
2022-10-11 22:36:35 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -18,14 +18,18 @@
         >maelezo
       </p>
     </div>
-    <FeedbackForm />
-    <JokeBlock />
+    
   </main>
 </template>
 
 <style>
 * {
   font-family: 'Helvetica', sans-serif;
+}
+.intro {
+  min-width: 400px;
+  padding: 10px 40px;
+  width: 60%;
 }
 
 </style>
